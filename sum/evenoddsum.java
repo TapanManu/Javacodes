7fd1c37bdd74f2e60f2cda2a3c8fc2a9069b859c
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-01-18 23:14:40 +0530

    Update evenoddsum.java

--- sum/evenoddsum.java
+++ sum/evenoddsum.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class evenoddsum{
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
-		int[] a= new int[20];
+		n=s.nextInt();
+		int[] a= new int[n];
 		int i,sumeven=0,sumodd=0,n;
 		System.out.println("number of array elements");
-		n=s.nextInt();
+		
 		for(i=0;i<n;i++){
 			a[i]=s.nextInt();
 		}
