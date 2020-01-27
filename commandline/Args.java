95a17e0453fe7b5e5f65cfc7cacdd49b8c5660af
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-01-27 22:37:31 +0530

    Create Args.java
    java Args this is a command line argument
    args[0]: this
    args[1]: is
    args[2]: a
    args[3]: command
    args[4]: line
    args[5]: argument

---
+++ commandline/Args.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Args {
+   public static void main(String args[]) {
+      for(int i = 0; i<args.length; i++) {
+         System.out.println("args[" + i + "]: " + args[i]);
+      }
+   }
+}
