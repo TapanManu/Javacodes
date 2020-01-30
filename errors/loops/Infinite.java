04433dcedea36793b6ca55138a94543b6bd59bdc
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-01-31 00:04:50 +0530

    Create Infinite.java
    infinite loop

---
+++ errors/loops/Infinite.java
@@ -0,0 +1,9 @@
+class Infinite{
+	public static void main(String [] args){
+		int i;
+		for(i=0;i<5;);
+		System.out.print(i);	
+	}
+}
+
+//The above program has no updation expression which results in infinite loop
