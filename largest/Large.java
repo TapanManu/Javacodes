dace60e20b60799341ec99e2680a5bbec55ee61c
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-03 19:51:39 +0530

    Update Large.java

--- largest/Large.java
+++ largest/Large.java
@@ -4,14 +4,14 @@ public class Large{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
 
-        int a,b,c,large=0;
+        double a,b,c,large=0;
 
         System.out.println("Enter the first number:");
-        a=s.nextInt();
+        a=s.nextDouble();
         System.out.println("Enter the second number:");
-        b=s.nextInt();
+        b=s.nextDouble();
         System.out.println("Enter the third number:");
-        c=s.nextInt();
+        c=s.nextDouble();
 
         if(a<b){
             large=b;
