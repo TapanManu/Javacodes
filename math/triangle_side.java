01b323c33d42512b5daa79517c93a487f08c5573
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-03 22:33:16 +0530

    Create triangle_side.java

---
+++ math/triangle_side.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Triangle{
+	public static void main(String[] args){
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter the sides:");
+		short a = scan.nextShort();
+		short b = scan.nextShort();
+		short c = scan.nextShort();
+		if((a+b>c)||(b+c>a)||(a+c>b)){
+			if((a==b)&&(b==c)){
+				System.out.println("equilateral Triangle");
+			}
+			else if((a==0)||(b==0)||(c==0)){
+				System.out.println("not a Triangle");
+			}
+			else if((a==b)||(b==c)||(c==a)){
+				System.out.println("Isoceles Triangle");
+			}
+			else if(a*a+b*b==c*c){
+				System.out.println("Right Triangle");
+			}
+			else{
+				System.out.println("Scalene Triangle");
+			}
+		}
+		else{
+			System.out.println("not a Triangle");
+		}
+		
+	}
+}	
