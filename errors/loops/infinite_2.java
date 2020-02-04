f38dc42db4bd8eb0f6e45a8d4cea675d00ffcd71
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 00:03:25 +0530

    Create infinite_2.java

---
+++ errors/loops/infinite_2.java
@@ -0,0 +1,8 @@
+class Loop
+        {
+            public static void main(String args[]) 
+            {
+                for(System.out.println("hello");;);
+                //infifnite loop
+            } 
+        }
