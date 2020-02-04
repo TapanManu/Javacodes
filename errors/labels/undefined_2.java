dc1e6542f0d41b5c0192bdc1f5106b1966cdca15
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-04 22:42:21 +0530

    Create undefined_2.java

---
+++ errors/labels/undefined_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class ErrorTest{
+	public static void main(String[] args){
+		int i=1,j=1,k=1;
+		l1:{
+			i++;
+			System.out.println(i);
+			break l2;
+		}
+		l2:{
+			j++;
+			System.out.println(j);
+		
+		l3:{
+			k++;
+			System.out.println(k);
+
+		}
+	}
+}
+}
