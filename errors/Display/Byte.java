36aa4d9965efd4db665439ef4c693d7ef93ac513
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-04 23:51:05 +0530

    Create Byte.java

---
+++ errors/Display/Byte.java
@@ -0,0 +1,8 @@
+class Byte{
+public static void main(String args[]){
+byte b=50;
+b=b*50;
+}
+
+//While evaluating an expression containing int, bytes or shorts, 
+//the whole expression is converted to int then evaluated and the result is also of type int.
