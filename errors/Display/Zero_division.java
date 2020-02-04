753a72daf469332adce831f54f7e67673bd5ea5e
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-04 23:35:26 +0530

    Create Zero_division.java

---
+++ errors/Display/Zero_division.java
@@ -0,0 +1,25 @@
+    class output {
+
+            public static void main(String args[]) 
+
+            {
+
+                double a, b,c;
+
+                a = 3.0/0;                //displays infinity
+
+                b = 0/4.0;                //zero
+
+                c=0/0.0;                  //NaN
+
+     
+
+    	    System.out.println(a);
+
+                System.out.println(b);
+
+                System.out.println(c);
+
+            } 
+
+        }
