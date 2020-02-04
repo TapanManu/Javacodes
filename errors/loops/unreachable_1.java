cae62a0ab078979b2cc3e70ad359a744c8edd018
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 00:02:10 +0530

    Create unreachable_1.java

---
+++ errors/loops/unreachable_1.java
@@ -0,0 +1,9 @@
+class Loop
+        {
+            public static void main(String args[]) 
+            {
+                for(System.out.println("hello");false;);
+                
+                //; ==> unreachable statement
+            } 
+        }
