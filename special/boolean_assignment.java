0939bc4ebf76bdd314635ecb79e18c56aafa9a4a
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:30:09 +0530

    Create boolean_assignment.java

---
+++ special/boolean_assignment.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	Boolean t=true;
+	 	t=false;
+	 	if(t=true)
+	 		System.out.println("helloif");
+	}	
+}
+
+//no error 
+//output:helloif
