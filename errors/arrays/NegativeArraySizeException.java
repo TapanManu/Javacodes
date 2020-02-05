9187ea6899f12d31371c1ccf2f646f73109fb792
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 20:28:46 +0530

    Create NegativeArraySizeException.java

---
+++ errors/arrays/NegativeArraySizeException.java
@@ -0,0 +1,8 @@
+class Main{
+	 public static void main(String[] args) {
+			int[] a=new int[-5];
+    }
+}
+
+//Exception in thread "main" java.lang.NegativeArraySizeException
+//	at Main.main(Main.java:3)
