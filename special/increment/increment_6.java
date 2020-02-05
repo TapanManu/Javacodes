00d76290f65836452f9b65eba1e5f6c936daf204
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 22:35:20 +0530

    Create increment_6.java

---
+++ special/increment/increment_6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	int a=0;
+	 	int b=a++ + --a;
+	 	//a++ is 0 
+	 	//a becomes 1
+	 	//--a 0
+	 	//0+0=0
+	 	System.out.println(b);
+	}	
+}
