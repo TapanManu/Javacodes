10f328f22e132efba7fe05191500d4dc64dd630d
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:25:26 +0530

    Create type_incompatability_2.java

---
+++ errors/selection/type_incompatability_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	if("true")
+	 		System.out.println("helloif");
+	}	
+}
+
+//error: incompatible types: String cannot be converted to boolean
+	 	if("true")
+	 	   ^
+1 error
