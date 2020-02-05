318135755033bd88eb058764fd7b397a58efe3dc
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:18:46 +0530

    Create illegalexpressionstart.java

---
+++ errors/selection/illegalexpressionstart.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	if()
+	 		System.out.println("helloif");
+	 	else
+	 		System.out.println("helloelse");
+	}	
+}
+
+
+ error: illegal start of expression
+	 	if()
+	 	   ^
+1 error
