b4dff26c96cae3c78272e28c93af37c310bfaf97
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:34:09 +0530

    Create bad_operand_type_2.java

---
+++ errors/selection/bad_operand_type_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	Boolean True=false;
+	 	Boolean False=true;
+	 	if(True++)
+	 		System.out.println("helloif");
+	}	
+}
+
+//error: bad operand type Boolean for unary operator '++'
+	 	if(True++)
+	 	       ^
+1 error
