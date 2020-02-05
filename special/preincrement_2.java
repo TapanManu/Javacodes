b060cf9b8d239cbd89131d245894416bd26a2df4
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:42:40 +0530

    Create preincrement_2.java

---
+++ special/preincrement_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	int a=-1;
+	 	a++;
+	 	System.out.println(a);
+	 	if(1==++a)
+	 		System.out.println("helloif");
+	 	else
+	 		System.out.println(a);
+	}	
+}
