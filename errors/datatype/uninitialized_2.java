f1ffab2334498c0e79421aa809c4907c3a008fc3
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 22:43:38 +0530

    Create uninitialized_2.java

---
+++ errors/datatype/uninitialized_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	int a;
+	 	a=a=1*a;
+	 	//a uninitialized
+	 	//first 1*a is evaluated
+	 	//a is not initialized
+	 	System.out.println(a);
+	}	
+}
