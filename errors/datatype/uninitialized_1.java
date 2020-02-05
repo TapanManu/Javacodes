e519da6392890cf82950ecc5a3ae2bc2153908f7
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:37:59 +0530

    Create uninitialized_1.java

---
+++ errors/datatype/uninitialized_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	int a;
+	 	a++;
+	 	if(a==1)
+	 		System.out.println("helloif");
+	}	
+}
+variable a might not have been initialized
+	 	a++;
+	 	^
+
