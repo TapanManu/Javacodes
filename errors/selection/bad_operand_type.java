b409e639760f2321a8acf7f68c23a2e06b77f23a
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:28:17 +0530

    Create bad_operand_type.java

---
+++ errors/selection/bad_operand_type.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	Boolean t=true;
+	 	t=false;
+	 	if(t<true)
+	 		System.out.println("helloif");
+	}	
+}
+
+
+//error: bad operand types for binary operator '<'
+	 	if(t<true)
+	 	    ^
+  first type:  Boolean
+  second type: boolean
+1 error
