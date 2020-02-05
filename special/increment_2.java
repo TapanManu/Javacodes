358666f076a874ce676e267291d291ade84a9ded
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 22:16:39 +0530

    Create increment_2.java

---
+++ special/increment_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	int a=1,b;
+	 	b=a++;
+	 	System.out.println(b);
+	 	System.out.println(a++);
+	 	System.out.println(a);
+	 	System.out.println(++a);
+	}	
+}
+java Main
+1
+2
+3
+4
