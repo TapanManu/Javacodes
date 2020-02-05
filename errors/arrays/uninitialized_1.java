3b2737220e894edaaafa53b6477d4a962e17cfe8
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 20:39:01 +0530

    Create uninitialized_1.java

---
+++ errors/arrays/uninitialized_1.java
@@ -0,0 +1,12 @@
+class Main{
+	 public static void main(String[] args) {
+	 		int c;
+			int[] a=new int[c];
+			
+	}
+}
+
+//Main.java:4: error: variable c might not have been initialized
+			int[] a=new int[c];
+			                ^
+1 error
