fea048844268ba731f314092a0f09b9b5148d97d
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:10:47 +0530

    Create type_incompatability_1.java

---
+++ errors/selection/type_incompatability_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 if(System.out.println("hi"));
+	}
+}
+
+
+
+//Main.java:4: error: incompatible types: void cannot be converted to boolean
+	 if(System.out.println("hi"));
+	                      ^
+1 error
