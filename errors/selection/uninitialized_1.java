38d0ad1d180d075a1e77f040150481e5aae4c66c
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:19:53 +0530

    Create uninitialized_1.java

---
+++ errors/selection/uninitialized_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	Boolean t;
+	 	if(t)
+	 		System.out.println("helloif");
+	 	else
+	 		System.out.println("helloelse");
+	}	
+}
+
+// error: variable t might not have been initialized
+	 	if(t)
+	 	   ^
+1 error
