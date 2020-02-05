9172fb6b6fd6b6e1a75987ef5c98c1f8a7c65406
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:35:42 +0530

    Create unexpected_1.java

---
+++ errors/datatype/unexpected_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	int a=5++;
+	 	if(a==6)
+	 		System.out.println("helloif");
+	}	
+}
+
+// error: unexpected type
+	 	int a=5++;
+	 	      ^
+  required: variable
+  found:    value
