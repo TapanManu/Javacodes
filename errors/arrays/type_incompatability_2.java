3021fdcc5188eabb9dbe1e0ded88a2fca0cf7604
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 20:31:33 +0530

    Create type_incompatability_2.java

---
+++ errors/arrays/type_incompatability_2.java
@@ -0,0 +1,12 @@
+class Main{
+	 public static void main(String[] args) {
+			int[] a=new int[2.3];
+			
+	}
+}
+
+
+//Main.java:3: error: incompatible types: possible lossy conversion from double to int
+			int[] a=new int[2.3];
+			                ^
+1 error
