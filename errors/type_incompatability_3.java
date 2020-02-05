8a6d438049ced25c3d7629ce5ccce3a7f771cbef
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 20:36:20 +0530

    Create type_incompatability_3.java

---
+++ errors/type_incompatability_3.java
@@ -0,0 +1,14 @@
+class Main{
+	 public static void main(String[] args) {
+	 		String c;
+			int[] a=new int[c];
+			
+	}
+}
+
+
+
+//Main.java:4: error: incompatible types: String cannot be converted to int
+			int[] a=new int[c];
+			                ^
+1 error
