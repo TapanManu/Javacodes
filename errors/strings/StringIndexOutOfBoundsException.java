9836ae14e5de887f76bd3a4f1ad89784401b5c18
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 21:01:58 +0530

    Create StringIndexOutOfBoundsException.java

---
+++ errors/strings/StringIndexOutOfBoundsException.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	 char ch=sc.next().charAt(1);       //error if only one char input given
+	 System.out.println(ch);
+	}
+}
+
+//~$ java Main
+w
+Exception in thread "main" java.lang.StringIndexOutOfBoundsException: String index out of range: 1
+	at java.lang.String.charAt(String.java:658)
+	at Main.main(Main.java:5)
