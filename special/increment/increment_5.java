c058623f1ca9ccfe8580d0ec4328067865ce0f5e
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-05 22:32:59 +0530

    Create increment_5.java

---
+++ special/increment/increment_5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+	 public static void main(String[] args) {
+	 	int a=0;
+	 	a=a++;
+	 	System.out.println(a);
+	 	a=++a;
+	 	System.out.println(a);
+	}	
+}
+//output
+//0
+//1
