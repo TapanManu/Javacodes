4d2a340dd9f8d51caf7b9b49749d0365d0f0037e
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 22:00:48 +0530

    Create conversion_2.java

---
+++ special/display/conversion_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+		int a=5;
+		Scanner sc=new Scanner(System.in);
+		System.out.print('a'+'b');
+	}
+}
+
+//output
+///195
