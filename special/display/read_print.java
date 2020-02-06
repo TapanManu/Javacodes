554e9fe0e7bbdda7284a2c1abebbc349063a89e6
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 21:41:24 +0530

    Create read_print.java

---
+++ special/display/read_print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+		String s;
+		Scanner sc=new Scanner(System.in);
+		System.out.println(s=sc.nextLine());
+	}
+}
+//reads and prints
