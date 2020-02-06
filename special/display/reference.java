25e451d951f917494814a52097ca520b997b62c5
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 21:56:49 +0530

    Create reference.java

---
+++ special/display/reference.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+		int a=5;
+		Scanner sc=new Scanner(System.in);
+		System.out.print(new int[]{1,2,3,4,5});
+	}
+}
+//output
+//[I@a14482
