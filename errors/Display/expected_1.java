9ebef27b13aab289e25ffad1552f77ddf57aa641
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 21:51:36 +0530

    Create expected_1.java

---
+++ errors/Display/expected_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+		int a=5;
+		Scanner sc=new Scanner(System.in);
+		System.out.print(int a=3);
+	}
+}
+
+main.java:6: error: '.class' expected
+		System.out.print(int a=3);
+		                     ^
+main.java:6: error: ';' expected
+		System.out.print(int a=3);
+		                        ^
+2 errors
