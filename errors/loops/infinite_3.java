60a1c12fc9577f6700b0ca6cb24d73a58047c906
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 23:24:22 +0530

    Create infinite_3.java

---
+++ errors/loops/infinite_3.java
@@ -0,0 +1,10 @@
+
+class Main{
+	public static void main(String[] args){
+		Boolean t=true;
+		int a=1,b=2;
+		for(System.out.println(a++);t;System.out.println(++a));
+	}
+}
+//output
+//1 to upto max integer variable it can hold
