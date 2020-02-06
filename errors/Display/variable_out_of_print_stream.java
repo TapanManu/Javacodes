e542c33e9432e8eb3a5a89be469fc1b70c67bcbb
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 22:15:13 +0530

    Create variable_out_of_print_stream.java

---
+++ errors/Display/variable_out_of_print_stream.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String[] args){
+		int a=5,b=3,c=4;
+		Scanner sc=new Scanner(System.in);
+		if(true)
+		{
+			System.out.prinln(++a);
+		}
+	}
+}
+//cannot find symbol
+			System.out.prinln(++a);
+			          ^
+  symbol:   method prinln(int)
+  location: variable out of type PrintStream
+1 error
