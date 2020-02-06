7e44fb321924d3f87a0e99144cf5aa5d3def157b
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 22:25:27 +0530

    Create bad_operand_type_1.java

---
+++ errors/operators/bad_operand_type_1.java
@@ -0,0 +1,15 @@
+class Main{
+	public static void main(String[] args){
+		int a=5;
+		if(a+=2==7)
+		{
+			System.out.println(a);
+		}
+	}
+}
+// bad operand types for binary operator '+'
+		if(a+=2==7)
+		    ^
+  first type:  int
+  second type: boolean
+1 error
