8eb3dce37f5536ceaa4e9e6c4b77ec6c9494a769
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 23:34:13 +0530

    Create identifier_expected.java

---
+++ errors/loops/identifier_expected.java
@@ -0,0 +1,11 @@
+class Main{
+	public static void main(String[] args){
+		for(int i=0,char b='1';i<=3;i++){
+			System.out.println(b)
+		}
+	}
+}
+//error: <identifier> expected
+		for(int i=0,char b='1';i<=3;i++){
+		            ^
+
