62c94b1c3c9eb205ee75aa4de5b5393e1b8c6c82
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-06 22:38:17 +0530

    Create unexpected_2.java

---
+++ errors/datatype/unexpected_2.java
@@ -0,0 +1,11 @@
+class Main{
+	public static void main(String[] args){
+		System.out.println(0+=2);
+	}
+}
+error: unexpected type
+		System.out.println(0+=2);
+		                   ^
+  required: variable
+  found:    value
+1 error
