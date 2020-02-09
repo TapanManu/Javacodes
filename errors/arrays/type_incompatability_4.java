fccc9f75e461c52cc228aae497ee076679909841
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-09 19:22:29 +0530

    Create type_incompatability_4.java

---
+++ errors/arrays/type_incompatability_4.java
@@ -0,0 +1,18 @@
+import java.lang.Character;
+class Main{
+	public static void main(String[] args){
+		int i=0;
+	for(int a[]={1,2,3,4};i<a.length;i++){
+		System.out.print(i[a]);
+	}
+}
+}
+
+//main.java:6: error: incompatible types: int[] cannot be converted to int
+		System.out.print(i[a]);
+		                   ^
+main.java:6: error: array required, but int found
+		System.out.print(i[a]);
+		                  ^
+2 errors
+
