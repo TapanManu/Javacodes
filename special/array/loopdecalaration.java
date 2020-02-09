ffa1138d865d5f96b76a7b5fbc52b46c26e82131
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-09 19:20:52 +0530

    Create loopdecalaration.java

---
+++ special/array/loopdecalaration.java
@@ -0,0 +1,11 @@
+import java.lang.Character;
+class Main{
+	public static void main(String[] args){
+		int i=0;
+	for(int a[]={1,2,3,4};i<a.length;i++){
+		System.out.print(a[i]);
+	}
+}
+}
+//output
+1 2 3 4
