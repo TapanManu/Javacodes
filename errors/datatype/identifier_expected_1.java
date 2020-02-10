5b1d5bf479a21c18674d561bb67fffb273e329b9
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-10 20:38:33 +0530

    Create identifier_expected_1.java

---
+++ errors/datatype/identifier_expected_1.java
@@ -0,0 +1,27 @@
+class Main{
+	int i,char c,boolean b,double d;
+	public static void main(String[] args){
+		int p,q;
+		System.out.print(p+" "+q);
+}
+}
+javac main.java
+main.java:3: error: <identifier> expected
+	int i,char c,boolean b,double d;
+	      ^
+main.java:3: error: <identifier> expected
+	int i,char c,boolean b,double d;
+	            ^
+main.java:3: error: <identifier> expected
+	int i,char c,boolean b,double d;
+	             ^
+main.java:3: error: <identifier> expected
+	int i,char c,boolean b,double d;
+	                      ^
+main.java:3: error: <identifier> expected
+	int i,char c,boolean b,double d;
+	                       ^
+main.java:3: error: <identifier> expected
+	int i,char c,boolean b,double d;
+	                               ^
+6 errors
