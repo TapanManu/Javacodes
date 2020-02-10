b6f7975cd80723d257330f9ce1ce85d290740e29
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-10 23:40:28 +0530

    Create private_class.java

---
+++ errors/class/private_class.java
@@ -0,0 +1,15 @@
+private class Main{
+	int i;char c;boolean b;double d;
+	static int count;	
+	static void main(String[] args){
+		Main ob1=new Main();
+		Main ob2=new Main();
+		int count=1;
+		System.out.print(count);
+		System.out.print(Main.count);
+		System.out.print(ob1.count);
+		System.out.print(ob2.count);
+
+}
+}
+//modifier private not allowed here
