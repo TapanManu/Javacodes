1a47173e4bae0265d6a8a7f8968a5b20131ee9c0
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-10 21:23:43 +0530

    Create instance_variables.java

---
+++ special/class/instance_variables.java
@@ -0,0 +1,13 @@
+class Main{
+	int i;char c;boolean b;double d;	
+	public static void main(String[] args){
+		Main ob1=new Main();
+		Main ob2=new Main();
+		System.out.println(ob1.i+" "+"c:"+ob1.c+" "+ob1.b+" "+ob1.d);
+		System.out.println(ob2.i+" "+"c:"+ob2.c+" "+ob2.b+" "+ob2.d);
+}
+}
+
+//output
+0 c: false 0.0
+0 c: false 0.0
