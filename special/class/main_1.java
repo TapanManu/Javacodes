a20a5b1f46ea03e8513360200bad0d4ebeb565fa
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-10 21:37:42 +0530

    Create main_1.java

---
+++ special/class/main_1.java
@@ -0,0 +1,10 @@
+class Main{
+	int i;char c;boolean b;double d;	
+	public static void main(String[] args){
+		Main ob1=new Main();
+		Main ob2=new Main();
+		ob1.i=1;ob1.c='a';ob1.b=true;ob1.d=3.0;
+		System.out.println(ob1.i+" "+"c:"+ob1.c+" "+ob1.b+" "+ob1.d);
+		System.out.println(ob2.i+" "+ob2.c+" "+ob2.b+" "+ob2.d);
+}
+}
