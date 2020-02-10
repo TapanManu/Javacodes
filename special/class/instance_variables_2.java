83f6010ac7305ef33c2041336722b80b40304eee
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-10 23:29:21 +0530

    Create instance_variables_2.java

---
+++ special/class/instance_variables_2.java
@@ -0,0 +1,15 @@
+class Main{
+	int i;char c;boolean b;double d;
+	static int count;	
+	public static void main(String[] args){
+		Main ob1=new Main();
+		Main ob2=new Main();
+		int count=1;
+		System.out.print(count);
+		System.out.print(Main.count);
+		System.out.print(ob1.count);
+		System.out.print(ob2.count);
+
+}
+}
+//1 0 0 0 
