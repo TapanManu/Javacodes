64d11375ddac7a715bf6da9396d0db1bc09a3490
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-10 23:41:56 +0530

    Create private_class_2.java

---
+++ errors/class/private_class_2.java
@@ -0,0 +1,14 @@
+private class Main{
+	int i;char c;boolean b;double d;
+	static int count;	
+	private static void main(String[] args){
+		Main ob1=new Main();
+		Main ob2=new Main();
+		int count=1;
+		System.out.print(count);
+		System.out.print(Main.count);
+		System.out.print(ob1.count);
+		System.out.print(ob2.count);
+
+}
+}
