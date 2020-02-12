7bf4a6c28401946d2c9193ddf877e86f218e088e
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-12 21:22:36 +0530

    Create methods_2.java

---
+++ special/class/methods_2.java
@@ -0,0 +1,26 @@
+
+public class A
+{
+    static int methodOne(int i)
+    {
+        i = i++ + --i;
+ 
+        return i;
+    }
+ 
+    static int methodTwo(int i)
+    {
+        i = i-- - ++i + methodOne(i);
+ 
+        return i;
+    }
+ 
+    public static void main(String[] args)
+    {
+        int i = 11;
+ 
+        System.out.println(methodTwo(i) + ++i - --i);
+    }
+}
+//output
+23
