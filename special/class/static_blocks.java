4d104f8ca122619765d00897a7ee4ad1c1d358f0
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-12 20:41:10 +0530

    Create static_blocks.java

---
+++ special/class/static_blocks.java
@@ -0,0 +1,32 @@
+class A
+{
+    static int a = 50;
+ 
+    static
+    {
+        a = 50;
+    }
+ 
+    static
+    {
+        a = 50;
+    }
+}
+
+//three static blocks
+
+Three.
+class A
+{
+    static int a = 50;       // ----- (1)
+ 
+    static
+    {
+        a = 50;             // ----- (2)
+    }
+ 
+    static
+    {
+        a = 50;            // ----- (3)
+    }
+}
