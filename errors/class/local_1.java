28d3452690dfed8abafb722a671955e06cc6221b
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-12 21:19:57 +0530

    Create local_1.java

---
+++ errors/class/local_1.java
@@ -0,0 +1,12 @@
+
+public class A
+{
+    public static void main(String[] args)
+    {
+        int i;
+ 
+        System.out.println(i);
+    }
+}
+//Because, local variable ‘i’ has been used before it is initialized.
+Local variables in java must get a value before they are used. Otherwise compiler will throw error.
