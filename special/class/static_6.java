60b19240f28998c6c8321063e32b9446818c9204
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-12 20:51:34 +0530

    Create static_6.java

---
+++ special/class/static_6.java
@@ -0,0 +1,21 @@
+public class A
+{
+    static
+    {
+        i = 30;
+    }
+ 
+    static int i = 20;
+ 
+    static
+    {
+        i = 10;
+    }
+ 
+    public static void main(String[] args)
+    {
+        System.out.println(A.i);
+    }
+}
+//output
+10
