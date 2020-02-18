ce0121f565624f76ac2fea6c737b299dd7b93729
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 23:15:32 +0530

    Create relational1.java

---
+++ operators/relational1.java
@@ -0,0 +1,10 @@
+class Relational_operator 
+    {
+        public static void main(String args[])
+        {
+            int var1 = 5; 
+            int var2 = 6;
+            System.out.print(var1 > var2);
+        } 
+    }
+    //false
