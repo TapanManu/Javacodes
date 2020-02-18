73cb71e50686d8ad4af3df01af8e0aa3d6558085
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 23:23:57 +0530

    Create ternary.java

---
+++ operators/ternary.java
@@ -0,0 +1,12 @@
+class ternary_operator 
+    {
+        public static void main(String args[]) 
+        {        
+             int x = 3;
+             int y = ~ x;
+             int z;
+             z = x > y ? x : y;
+             System.out.print(z);
+        } 
+    }
+    //3
