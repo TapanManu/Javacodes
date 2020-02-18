352ed84cc03afba5c65b815acf64c8f30a34fac7
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 23:13:57 +0530

    Create bitwise_op3.java

---
+++ operators/bitwise_op3.java
@@ -0,0 +1,15 @@
+class Output 
+{
+    public static void main(String args[]) 
+    {    
+         int a = 1;
+         int b = 2;
+         int c = 3;
+         a |= 4;
+         b >>= 1;
+         c <<= 1;
+         a ^= c;
+         System.out.println(a + " " + b + " " + c);
+    } 
+}
+//3 1 6
