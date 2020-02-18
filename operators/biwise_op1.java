b12d16e15c5267f8c5420c249f13f764acf92f1d
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 22:54:09 +0530

    Create biwise_op1.java

---
+++ operators/biwise_op1.java
@@ -0,0 +1,10 @@
+class bitwise_operator 
+{
+    public static void main(String args[])
+    {
+        int var1 = 42;
+        int var2 = ~var1;
+        System.out.print(var1 + " " + var2);     	
+    } 
+}
+//42 -43
