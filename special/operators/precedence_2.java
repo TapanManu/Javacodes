140b5d8fe521d3a57ec23d463484b1ed1f6bacca
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-19 00:29:03 +0530

    Create precedence_2.java

---
+++ special/operators/precedence_2.java
@@ -0,0 +1,10 @@
+class Output 
+{
+        public static void main(String args[]) 
+        {    
+            int a=4, b=8; 
+            boolean c = a>2 ^ b<10 & false;
+             System.out.println(c); 
+        } 
+}
+//true
