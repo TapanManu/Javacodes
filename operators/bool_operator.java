776a6299f1aeec60bcf03352a5327e17f253eca3
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 23:20:11 +0530

    Create bool_operator.java

---
+++ operators/bool_operator.java
@@ -0,0 +1,13 @@
+ class bool_operator 
+    {
+        public static void main(String args[]) 
+        {    
+             boolean a = true;
+             boolean b = !true;
+             boolean c = a | b;
+ 	     boolean d = a & b;
+             boolean e = d ? b : c;
+             System.out.println(d + " " + e);
+        } 
+    }
+    //false true
