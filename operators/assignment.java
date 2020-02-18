56a4dce59239adb34b482d0cca12c24e00f1be04
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 23:41:16 +0530

    Create assignment.java

---
+++ operators/assignment.java
@@ -0,0 +1,12 @@
+lass Output 
+{
+        public static void main(String args[]) 
+        {    
+             int a,b,c,d;
+             a=b=c=d=20
+            a+=b-=c*=d/=20
+           System.out.println(a+" "+b+" "+c+" "+d);
+ 
+        } 
+}
+//20 0 20 1
