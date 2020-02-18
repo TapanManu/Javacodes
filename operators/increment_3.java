10d9e18cd1a47d1ddac3dbf2e9d3722aa2c4e691
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 23:37:26 +0530

    Create increment_3.java

---
+++ operators/increment_3.java
@@ -0,0 +1,9 @@
+class operators 
+    {
+        public static void main(String args[]) 
+        {    
+             int x = 8;
+             System.out.println(++x * 3 + " " + x);
+        } 
+    }
+    //27 9
