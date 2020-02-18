a3d751c6bbd2b16415877e4dc73fdfb8ddcbdf07
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-18 23:05:45 +0530

    Create shift_op1.java

---
+++ operators/shift_op1.java
@@ -0,0 +1,11 @@
+class rightshift_operator 
+    {
+        public static void main(String args[]) 
+        {    
+             int x; 
+             x = 10;
+             x = x >> 1;
+             System.out.println(x);
+        } 
+    }
+    //5
