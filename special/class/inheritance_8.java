fd9917ebc2cd2080b9228ffd2455f58708853835
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-20 12:58:23 +0530

    Create inheritance_8.java

---
+++ special/class/inheritance_8.java
@@ -0,0 +1,79 @@
+class Poe extends Kylo {
+    public static void method2() {
+       //super.method2();
+        System.out.print("Poe 2   ");
+    }
+
+    public String toString() {
+        return "Poe " + super.toString();
+    }
+}
+
+  class Kylo extends Finn {
+    public static void method1() {
+        System.out.print("Kylo 1   ");
+    }
+      public String toString() {
+        return "Kylo";
+    }
+}
+
+  class Finn extends Rey {
+    public static  void method2() {
+        System.out.print("Finn 2   ");
+        method1();
+    }
+}
+
+  class Rey {
+    public String toString() {
+        return "Rey";
+    }
+
+    public static void method1() {
+        System.out.print("Rey 1   ");
+    }
+     public static void method2() {
+        System.out.print("Rey 2   ");
+    }
+}
+
+
+public class Inheritance
+{
+  public static void main(String[] args) {
+Rey[] elements = { new Finn(), new Rey(), new Poe(), new Kylo() };
+//OBjects of Rey being initialized as Rey is parent class,else
+//it would generate type-incompatibility error!
+for (int i = 0; i < elements.length; i++) {
+    System.out.println(elements[i]);
+    elements[i].method1();
+    System.out.println();
+    elements[i].method2();
+   System.out. println();
+    System.out.println();
+  System.out.println("=========================");
+
+} 
+}
+}
+Rey
+Rey 1   
+Finn 2   Rey 1   
+
+=========================
+Rey
+Rey 1   
+Rey 2   
+
+=========================
+Poe Kylo
+Kylo 1   
+Finn 2   Kylo 1   Poe 2   
+
+=========================
+Kylo
+Kylo 1   
+Finn 2   Kylo 1   
+
+=========================
