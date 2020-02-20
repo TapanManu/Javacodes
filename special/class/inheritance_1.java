076bf885703a9ce830d6062ddb70cfb5539443e2
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-20 10:55:26 +0530

    Create inheritance_1.java

---
+++ special/class/inheritance_1.java
@@ -0,0 +1,59 @@
+class A extends B {
+    public void method2() {
+        System.out.print("a 2  ");
+        method1();
+    }
+} 
+ class B extends C {
+    public String toString() {
+        return "b";
+    }
+    public void method2() {
+        System.out.print("b 2  ");
+        super.method2();
+    }
+}
+class C {
+    public String toString() {
+        return "c";
+    }
+    public void method1() {
+        System.out.print("c 1  ");
+    }
+    public void method2() {
+        System.out.print("c 2  ");
+    }
+}
+class D extends B {
+    public void method1() {
+        System.out.print("d 1  ");
+        method2();
+    }
+}
+public class Inheritance
+{
+  public static void main(String[] args) {
+        
+ C[] elements = {new A(), new B(), new C(), new D()};
+ for (int i = 0; i < elements.length; i++) {
+    System.out.println(elements[i]);
+    elements[i].method1();
+    System.out.println();
+
+        System.out.println("=========================");
+    }
+}
+}
+//output
+b
+c 1  
+=========================
+b
+c 1  
+=========================
+c
+c 1  
+=========================
+b
+d 1  b 2  c 2  
+=========================
