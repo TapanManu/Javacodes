b2908f8fab655778db3fb4f17d055c0888d2795e
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-20 20:06:38 +0530

    Create custom_print.java

---
+++ Strings/disp/custom_print.java
@@ -0,0 +1,10 @@
+package disp;
+public class custom_print{
+    public static void SOP(String args){
+        System.out.print(args);
+    }
+    public static void main(String [] args){
+        String a="Hello World";
+        SOP(a);
+    }
+}
