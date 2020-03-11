6991359b042709a45e710503af7728f248fea92a
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-03-11 17:03:33 +0530

    equal

---
+++ array/equals.java
@@ -0,0 +1,9 @@
+class CompArray{
+    public static void main(String[] args) {
+        int[] num={1,2,3};
+        int [] num1 = num;
+        if (num == num1){
+            System.out.println("equal");
+        }
+    }
+}
