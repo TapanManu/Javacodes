9ea8c342aa1933c28e10c634c4d28bf48780f310
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-03-11 16:16:42 +0530

    some comments added

--- Strings/disp/equals.java
+++ Strings/disp/equals.java
@@ -6,6 +6,8 @@ class Str{
         String name2 = "Tapan";
         if(name1 == name2){
             System.out.println("equal");
+            //hard coding produces equals to case-same memory address here!
+            //but input from the user results in not equal!as memory address are different here
         }
         else
         {
