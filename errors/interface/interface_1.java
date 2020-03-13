496b8e60c7c270f96b76a8890b1c661a81f7d113
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-03-13 11:09:36 +0530

    public not given to implemented functions

---
+++ errors/interface/interface_1.java
@@ -0,0 +1,61 @@
+interface Area{
+    void perimeter();
+}
+class Rectangle implements Area{
+    double length,breadth;
+    double p;
+    void perimeter(){
+        p = 2*(length+breadth);
+        System.out.println(p); 
+    }
+}
+class Circle implements Area{
+    final double PI = 3.14;
+    double radius,p;
+    void perimeter(){
+        p = 2 * PI * radius;
+        System.out.println(p); 
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.length = 5.0;
+        r.breadth = 3.0;
+        r.perimeter();
+        Circle c = new Circle();
+        c.radius = 5.0;
+        c.perimeter();
+    }
+}
+//interface Area{
+    void perimeter();
+}
+class Rectangle implements Area{
+    double length,breadth;
+    double p;
+    void perimeter(){
+        p = 2*(length+breadth);
+        System.out.println(p); 
+    }
+}
+class Circle implements Area{
+    final double PI = 3.14;
+    double radius,p;
+    void perimeter(){
+        p = 2 * PI * radius;
+        System.out.println(p); 
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.length = 5.0;
+        r.breadth = 3.0;
+        r.perimeter();
+        Circle c = new Circle();
+        c.radius = 5.0;
+        c.perimeter();
+    }
+}
+//attempting to access weaker priveleges,was public
