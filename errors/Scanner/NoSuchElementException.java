617173c423ec846a8663433619775e44e98d6cf3
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-03-16 22:16:55 +0530

    Error in scanner class
    Exception in thread "main" java.util.NoSuchElementException
            at java.base/java.util.Scanner.throwFor(Scanner.java:937)
            at java.base/java.util.Scanner.next(Scanner.java:1594)
            at java.base/java.util.Scanner.nextInt(Scanner.java:2258)
            at java.base/java.util.Scanner.nextInt(Scanner.java:2212)
            at Numbers.threeNum(Numbers.java:44)
            at Numbers.main(Numbers.java:49)

---
+++ errors/Scanner/NoSuchElementException.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Numbers{
+    int a,b,c;
+    Numbers(int num1,int num2){
+        a=num1;
+        b=num2;
+        this.disp(a,b);
+    }
+    Numbers(int n1,int n2,int n3){
+        a=n1;
+        b=n2;
+        c=n3;
+        this.disp(a,b,c);
+    }
+    void disp(int a,int b){
+        System.out.println("a+b is equal to "+(a+b));
+        System.out.println("a-b is equal to"+(a-b));
+        System.out.println("axb is equal to "+(a*b));
+        System.out.println("a/b is equal to "+(a/b)); //assume zero not given
+        System.out.println("a%b is equal to "+(a%b));
+    }
+    void disp(int a,int b,int c){
+        System.out.println("(a-b)/c is equal to "+((a-b)/c));
+        System.out.println("(a-c)/b is equal to "+((a-c)/b));
+        System.out.println("(b-a)/c is equal to "+((b-a)/c));
+        System.out.println("(b-c)/a is equal to "+((b-c)/a)); //assume zero not given
+        System.out.println("(c-a)/b is equal to "+((c-a)/b));
+        System.out.println("(c-b)/a is equal to "+((c-b)/a));
+    }
+    public static void twoNum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two numbers");
+        Numbers n = new Numbers(sc.nextInt(),sc.nextInt());
+        sc.close();
+    }
+    public static void threeNum(){
+        Numbers n;
+        int n1,n2,n3;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter three numbers");
+        //n1=sc.nextInt();
+        //n2=sc.nextInt();
+        //n3=sc.nextInt();
+        n = new Numbers(scan.nextInt(),scan.nextInt(),scan.nextInt());
+        scan.close();
+    }
+    public static void main(String[] args) {
+        twoNum();
+        threeNum();
+    }
+}
