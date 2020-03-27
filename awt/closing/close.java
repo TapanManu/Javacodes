7d9073b6ce87c12f721d34033ee9aea4b29392f7
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-03-27 12:27:15 +0530

    closing a window

---
+++ awt/closing/close.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+class MyFrame extends Frame implements WindowListener{
+	MyFrame(){
+		addWindowListener(this);
+		setSize(400,400);
+		setLayout(null);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new MyFrame();
+	}
+	public void windowActivated(WindowEvent e){System.out.println("Window Activated");}
+	public void windowClosed(WindowEvent e){
+		System.out.println("Window closed");
+		 }
+	public void windowClosing(WindowEvent e){
+		System.out.println("Window is closing");
+		dispose();
+	}
+    public void windowDeactivated(WindowEvent e){System.out.println("Window Deactivated");}
+    public void windowDeiconified(WindowEvent e){System.out.println("Window deiconified");}
+    public void windowIconified(WindowEvent e){System.out.println("Window iconified");}
+    public void windowOpened(WindowEvent e){System.out.println("Window Opened");}
+
+}
