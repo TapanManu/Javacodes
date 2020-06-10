b338dcfaa56be8cf7d16cfd0890450df26ee5e99
Tapan Manu <48797926+TapanManu@users.noreply.github.com>
2020-06-10 21:38:51 +0530

    first work

---
+++ JDBC/firstdb.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+ import java.sql.DriverManager;
+  class JDBCTest { 
+  	private static final String url = "jdbc:mysql://localhost";
+  	 private static final String user = "root";
+  	  private static final String password = "tapan*1234";
+  	   public static void main(String args[])
+  	    { try 
+  	    	{ 
+  	    		Connection con = DriverManager.getConnection(url, user, password); 
+  	    		System.out.println("Success"); 
+  	    	} 
+         catch (Exception e) 
+         { e.printStackTrace(); 
+         } 
+     } 
+ }
