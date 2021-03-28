ca038d1c9b95b5970a0764238cf53bc26d83ce37
Tapan Manu <48797926+TapanManu@users.noreply.github.com>
2021-03-28 13:55:40 +0530

    sample array deque

---
+++ Deque/ArrayDeque/sample.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
+class Block{
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<Integer>();
+        //Deque is interface, ArrayDeque is class
+
+        for(int i=0;i<20;i++){
+            deque.push(i);
+        }
+        Iterator d = deque.iterator();
+        deque.add(23);
+        while(d.hasNext()){
+            System.out.print(d.next()+" ");
+        }
+        deque.pop();
+        System.out.println();
+        d = deque.iterator();
+        while(d.hasNext()){
+            System.out.print(d.next()+" ");
+        }
+        deque.clear();
+        deque.addFirst(50);  //==> same as push()
+        //add() same as addLast()
+        System.out.println();
+        d = deque.iterator();
+        while(d.hasNext()){
+            System.out.print(d.next()+" ");
+        }
+    }
+}
